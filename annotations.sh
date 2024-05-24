c966f3cd4968084c93196bd2a22c4d94f40f68b2
mi3osz <145137756+mi3osz@users.noreply.github.com>
2024-05-24 21:06:37 +0200

    Update annotations.sh

--- annotations.sh
+++ annotations.sh
@@ -5,7 +5,7 @@ MODEL_VERSION="7b"
 REPO_DIR="/home/milosz/EMLI12"
 TARGET_FOLDER="annotations"
 COMMIT_MESSAGE="Update and standardize annotations in JSON files"
-SLEEP_INTERVAL=5 # Time to wait between checks (in seconds)
+SLEEP_INTERVAL=5
 
 mkdir -p "$REPO_DIR/$TARGET_FOLDER"
 
@@ -21,12 +21,11 @@ process_image() {
     # Create target directory if it doesn't exist
     mkdir -p "$target_dir"
 
-    # Check if JSON file exists and needs annotation
+    # Check for existance and annotation
     if [ -f "$json_file" ]; then
         # echo "Found existing JSON file: $json_file"
         if jq -e '.Annotation."New Source" | contains("Ollama:'$MODEL_VERSION'")' "$json_file" > /dev/null 2>&1; then
             # echo "Skipping $image_file; JSON file already annotated."
-            # Ensure the file in repo is up-to-date
             cp "$json_file" "$target_json_file"
             return
         else
@@ -38,14 +37,12 @@ process_image() {
         return
     fi
 
-    # Run Ollama only if needed
+    # Annotation Process
     local ollama_output=$(ollama run llava:${MODEL_VERSION} "describe $image_file")
     if [ $? -eq 0 ]; then
         # Annotate existing JSON file
         jq --arg version "Ollama:$MODEL_VERSION" --arg text "$ollama_output" \
            '.Annotation["New Source"] = $version | .Annotation.Test = $text' "$json_file" > tmp.$$ && mv tmp.$$ "$json_file"
-
-        # Copy the updated JSON file to the target directory
         cp "$json_file" "$target_json_file"
         # echo "JSON file updated successfully at $json_file and $target_json_file"
     else
@@ -59,22 +56,19 @@ export REPO_DIR
 export TARGET_FOLDER
 export ROOT_DIR
 
-# Infinite loop to continuously check for new images
 while true; do
-    # Find all JPG images in the directory and subdirectories and process each one
+    # Looking for every JPG and processing
     find "$ROOT_DIR" -type f -iname "*.jpg" -exec bash -c 'process_image "$0"' {} \;
 
-    # Change to the Git repository directory
+    
     cd "$REPO_DIR"
 
-    # Debugging: List JSON files in the target folder
-    # echo "Listing JSON files in $TARGET_FOLDER:"
     find "$TARGET_FOLDER" -type f -name "*.json"
 
     # Stage the new or updated JSON files
     git add "$TARGET_FOLDER"/*.json
 
-    # Commit the changes to the repository if there are any staged files
+    # Commit the changes to the repo
     if git diff-index --quiet HEAD --; then
         echo "No changes to commit."
     else
@@ -84,6 +78,6 @@ while true; do
 
      echo "All images processed and changes committed to the repository."
 
-    # Wait for the specified interval before checking again
+    # Wait and check again
     sleep $SLEEP_INTERVAL
 done
